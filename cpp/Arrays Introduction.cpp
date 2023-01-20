ddbb50024f2065de7697d5d8e60aeece21325caa
Munkhbadral1 <rizyl75@gmail.com>
2023-01-20 22:27:38 +0800

    cpp

---
+++ cpp/Arrays Introduction.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int arr[1000];
+    int a;
+    cin>>a;
+    for(int i=0; i<=a; i++){
+        cin>>arr[i];
+    }
+    
+    
+    return 0;
+}
