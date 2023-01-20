5f2afae8d7fa19f1efe4e9bba5792bcb962bf99a
Munkhbadral1 <rizyl75@gmail.com>
2023-01-20 22:32:11 +0800

    fail

--- cpp/Arrays Introduction.cpp
+++ cpp/Arrays Introduction.cpp
@@ -9,10 +9,10 @@ int main() {
     int arr[1000];
     int a;
     cin>>a;
-    for(int i=0; i<=a; i++){
+    for(int i=0; i<a; i++){
         cin>>arr[i];
     }
-    for(int c=0; c<=a; c++){
+    for(int c=0; c<a; a--){
         cout<<arr[c]<<" ";
     }
     
