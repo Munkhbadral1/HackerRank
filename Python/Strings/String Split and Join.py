c76a791d80987f1ceb1c19542430132ab6ef3c0f
Munkhbadral1 <rizyl75@gmail.com>
2023-01-22 00:42:05 +0800

    1

--- Python/Strings/String Split and Join.py
+++ Python/Strings/String Split and Join.py
@@ -2,7 +2,10 @@
 
 def split_and_join(line):
     # write your code here
-
+    result = line.split("-")
+    
+    result = "-".join(line)
+    
 if __name__ == '__main__':
     line = input()
     result = split_and_join(line)
