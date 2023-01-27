1fdefe07169a347e60360124717019c27ae2f617
Munkhbadral1 <rizyl75@gmail.com>
2023-01-27 16:50:04 +0800

    өбй

--- Java/Introduction/ Java Stdin and Stdout I.java
+++ Java/Introduction/ Java Stdin and Stdout I.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        // Complete this line
-        // Complete this line
-
+        Scanner scan = new Scanner(System.in); 
+        int a = scan.nextInt(); //a гэх int төрлийн хувьсагч. = ардах бол ер байнга байдаг тогтмол зүйл байх.
+        
+        scanner.close();        //байнга close гэдгийг ашигладаг байх.
         System.out.println(a);
         // Complete this line
         // Complete this line
     }
 }
+//Scanner class ashiglaj stdin
