d554c734f5e693b6321e0463781795e0d3397e68
Munkhbadral1 <rizyl75@gmail.com>
2023-01-27 16:53:42 +0800

    1

--- Java/Introduction/ Java Stdin and Stdout I.java
+++ Java/Introduction/ Java Stdin and Stdout I.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in); 
+        Scanner scan = new Scanner(System.in); //Scanner scanner = new Scanner(System.in); baij magadgvi
         int a = scan.nextInt(); //a geh int turliin huwisagch. = ardah bol bainga baidag baih
         int b = scan.nextInt();
         int c = scan.nextInt();
