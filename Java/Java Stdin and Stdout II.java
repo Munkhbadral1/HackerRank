058cdc2521f66d2988c4fd3958c146818d54b324
Munkhbadral1 <rizyl75@gmail.com>
2023-01-27 17:12:28 +0800

    done

--- Java/Java Stdin and Stdout II.java
+++ Java/Java Stdin and Stdout II.java
@@ -6,14 +6,14 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
         double d = scan.nextDouble();
+        //line 10, 11 do w/ ChatGPT. String deer scan.nextLine(); ym baina.
         scan.nextLine(); // consume the newline character after the double
-        String s = scan.next();
+        String s = scan.nextLine();
         
         
-
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
     }
 }
- 
+
